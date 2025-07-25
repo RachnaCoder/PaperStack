e4307805abe9d587f726b07582619939dd16002d
Rachna <rachna20032004@gmail.com>
2025-07-25 10:44:44 +0530

    first commit

---
+++ Frontend/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+<h1 class ="text-7xl   inline-flex hover:bg-sky-700  text-gray-500 font-bold bg-sky-500"> how are you</h1>
+
+    </div>
+  )
+}
+
+export default App
