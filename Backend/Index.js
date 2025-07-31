f50e0e2258f63919f2901d2df2aab1a28ebaa433
Rachna <rachna20032004@gmail.com>
2025-07-31 18:02:58 +0530

    dependencies setup

--- Backend/Index.js
+++ Backend/Index.js
@@ -4,4 +4,15 @@ import dotenv from "dotenv";
 dotenv.config({
   path: "./env"  
 })
-connectDB();
+
+connectDB()
+.then(()=>{
+app.listen(process.env.PORT || 8000 ,()=>{
+  console.log(`server is running on port  ${process.env.PORT}`);
+})
+})
+
+.catch((err)=>{
+console.log("Mongodb connection Failed", err);
+}
+)
