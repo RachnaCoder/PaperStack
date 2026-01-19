052f7bc3725ffb52c052a014466a10dbbef3397f
Rachna Yadav <rachna20032004@gmail.com>
2026-01-19 18:40:03 +0530

    cors updation

--- Backend/app.js
+++ Backend/app.js
@@ -6,10 +6,27 @@ import expressSession from "express-session";
 import {User} from "./Models/Users.model.js";
 
 const app = express();
+
+// app.use(cors({
+//   origin: process.env.CORS_ORIGIN,
+//   credentials: true  
+// }));
+
+
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true  
+  origin: function (origin, callback) {
+    console.log('Request Origin:', origin);  // Debug logs
+    if (!origin || origin === process.env.CORS_ORIGIN) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({
