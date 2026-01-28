04c3944d5e1eb7207d900e6a90241bd45fe1a2a7
Rachna Yadav <rachna20032004@gmail.com>
2026-01-29 02:20:07 +0530

    added vercel.json

--- Backend/app.js
+++ Backend/app.js
@@ -7,25 +7,26 @@ import {User} from "./Models/Users.model.js";
 
 const app = express();
 
-// app.use(cors({
-//   origin: process.env.CORS_ORIGIN,
-//   credentials: true  
-// }));
-
-
 app.use(cors({
-  origin: function (origin, callback) {
-    console.log('Request Origin:', origin);  // Debug logs
-    if (!origin || origin === process.env.CORS_ORIGIN) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+   origin: "*",
+    credentials: true ,
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+ }));
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     console.log('Request Origin:', origin);  // Debug logs
+//     if (!origin || origin === process.env.CORS_ORIGIN) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 
 
 app.use(express.json());
