5a55adaf1f9727c3c3c600a5f9479247a5cbff40
Rachna Yadav <rachna20032004@gmail.com>
2026-01-29 02:36:43 +0530

    some changes

--- Frontend/my-react-app/src/api.js
+++ Frontend/my-react-app/src/api.js
@@ -1,6 +1,6 @@
 // src/api.js - CORRECT VERSION
-export const API_BASE = import.meta.env.DEV 
-  ? 'http://localhost:8000' 
-  : 'https://paper-stack-inky.vercel.app/'; 
+export const API_BASE ='https://paper-stack-inky.vercel.app'; 
+  
+   
 
 
